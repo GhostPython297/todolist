5aa25c07b0737d3851456b67365057e6055ee08a
Ghost112358-dev <normando8643@gmail.com>
2025-08-07 21:14:50 -0300

    Atualização de login

--- src/main/java/com/ifpb/todolist/Controllers/TelaLoginController.java
+++ src/main/java/com/ifpb/todolist/Controllers/TelaLoginController.java
@@ -1,23 +1,47 @@
 package Controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class TelaLoginController {
-    // Sugestões: adicionar uma linha para digitar senha e um botão para criar conta caso não tenha
-    // ação IrParaTelaPrincipal deve sofrer alterações quando for decidido como o usuario fara login
-    @FXML
-    public void IrParaTelaPrincipal(ActionEvent event) throws Exception{
-        Parent tela2 = FXMLLoader.load(getClass().getResource("/view/TelaPrincipal.fxml"));
-        Scene scene2 = new Scene(tela2);
+import java.io.IOException;
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene2);
-        stage.setTitle("Visualização em lista");
+public class TelaLoginController {
+
+    @FXML
+    public TextField usename;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label mensagemLabel;
+
+    private final String loggedInUser = "Gabriel";
+    private final String loggedInPassword = "123456";
+
+
+    @FXML
+    public void IrParaTelaPrincipal() {
+        String nome = usename.getText();
+        String senha = password.getText();
+
+        if (nome.equals(loggedInUser) && senha.equals(loggedInPassword)) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/telaPrincipal.fxml"));
+                Parent root = loader.load();
+                Stage stage = (Stage) usename.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Tela Principal");
+            } catch (IOException e) {
+                mensagemLabel.setText("Erro ao carregar a tela.");
+                e.printStackTrace();
+            }
+        } else {
+            mensagemLabel.setText("Email ou senha incorretos.");
+        }
     }
 }
